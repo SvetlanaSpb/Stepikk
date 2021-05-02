df15b29482513fda3987578b849f1a5188b699c4
Svetlana <89111378147@yandex.ru>
2021-05-02 12:40:38 +0300

    python lesson6_step11

--- 2604.py
+++ 2604.py
@@ -1,9 +1,15 @@
 import math
-x=(math.ceil(math.pow(math.pi, math.e)*10000))
+from selenium import webdriver
+
+
+try:
+    link = " http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+def calc(x):
+  return str(math.log(abs(12 * math.sin(int(x)))))
+x_element = browser.find_element_by_id("input_value")
+x = x_element.text
+y = calc(x)
 print(x)
-
-
-
-
-
-
