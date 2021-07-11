66ed2c750995c03bc2b334ebf8882c5bf9532f7e
Svetlana <89111378147@yandex.ru>
2021-07-12 01:37:44 +0300

    принемаем alert

---
+++ pythonlesson15.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import time
+import math
+
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input = browser.find_element_by_id("answer")
+    input.send_keys(y)
+
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(10)
+    browser.quit()
